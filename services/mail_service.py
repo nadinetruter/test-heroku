1e4539cb37cff51b73d8c0dcbd8c2e036789c98f
nadine <nadinetruter02@gmail.com>
2021-01-14 14:53:43 -0800

    test

--- services/mail_service.py
+++ services/mail_service.py
@@ -15,9 +15,9 @@ from app import mail
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message("email sent", sender="bytecare0@gmail.com", recipients=User.email)
-    msg.body = text_body
-    msg.html = html_body
+    msg = Message("email sent", sender="bytecare0@gmail.com", recipients=recipients)
+    #msg.body = text_body
+    #msg.html = html_body
     mail.send(msg)
     #Thread(target=send_async_email, args=(app, msg)).start()
     return "Email sent"
