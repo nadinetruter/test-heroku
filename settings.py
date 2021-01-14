f72b351966101412b3f993b0c9903a3ac990edb0
nadine <nadinetruter02@gmail.com>
2021-01-14 11:40:17 -0800

    testing mail server

--- settings.py
+++ settings.py
@@ -8,6 +8,6 @@ load_dotenv(dotenv_path)
 
 SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 MAIL_SERVER = os.environ.get("MAIL_SERVER")
-MAIL_PORT = os.environ.get("MAIL_PORT")
+#MAIL_PORT = os.environ.get("MAIL_PORT")
 MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
