8dacfb3e2b16402aa999e3bb158c18901f34c607
nadine <nadinetruter02@gmail.com>
2021-01-14 16:08:30 -0800

    working mail

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ from resources.errors import errors
 
 application = app = Flask(__name__)
 app.config.from_pyfile('settings.py')
-app.config["DEBUG"]= True
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USERNAME"] = "bytecare0@gmail.com"
-app.config["MAIL_PASSWORD"] = "teambytecare0"
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USE_SSL"] = False
-app.config["MAIL_DEFAULT_SENDER"]="bytecare0@gmail.com"
+#app.config["DEBUG"]= True
+#app.config["MAIL_SERVER"] = "smtp.gmail.com"
+#app.config["MAIL_PORT"] = 587
+#app.config["MAIL_USERNAME"] = "bytecare0@gmail.com"
+#app.config["MAIL_PASSWORD"] = "teambytecare0"
+#app.config["MAIL_USE_TLS"] = True
+#app.config["MAIL_USE_SSL"] = False
+#app.config["MAIL_DEFAULT_SENDER"]="bytecare0@gmail.com"
 
 mail = Mail(app)
 
